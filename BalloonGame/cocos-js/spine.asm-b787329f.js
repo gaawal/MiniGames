8d33524f0a3fd6246875d22a7b12dc63fed0682f
jiahualink <840132699@qq.com>
2025-09-27 23:22:41 +0800

    commit

---
+++ BalloonGame/cocos-js/spine.asm-b787329f.js
@@ -0,0 +1,13 @@
+System.register([], (function (exports) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			exports('default', spine_asm);
+
+			function spine_asm () {}
+
+		})
+	};
+}));
+//# sourceMappingURL=spine.asm-b787329f.js.map
